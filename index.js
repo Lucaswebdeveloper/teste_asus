ab47048f5786e4cb58e516a820f4eff1b0dab8c0
Lucaswebdeveloper <62080207+Lucaswebdeveloper@users.noreply.github.com>
2020-11-20 11:01:53 -0300

    teste-asus-placa-mae
    home pages asus

---
+++ index.js
@@ -0,0 +1,10 @@
+//Arquivos SSAS
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+//meus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
